9fbaad13489b0d4f4eb9ccd44725adc3fea3cfed
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-04-20 18:10:48 -0300

    Jwt e Cors

--- index.js
+++ index.js
@@ -2,16 +2,78 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const mongo = process.env.MONGO || 'mongodb://localhost/minhas-series-rest'
+const User = require('./models/user')
+const jwt = require('jsonwebtoken')
+const jwtSecret = 'abc123abc123abc123'
+
+const cors = require('cors')
 
 const bodyParser = require('body-parser')
-app.use(bodyParser({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors({
+  // origin: 'http://server2:8080'
+  origin: (origin, callback) => {
+    if (origin === 'http://server2:8080') {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}))
 
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const series = require('./routes/series')
+const users = require('./routes/users')
 
 app.use('/series', series)
+app.use('/users', users)
+
+app.post('/auth', async (req, res) => {
+  const user = req.body
+  const userDb = await User.findOne({ username: user.username })
+  if (userDb) {
+    if (userDb.password === user.password) {
+      const payload = {
+        id: userDb._id,
+        username: userDb.username,
+        roles: userDb.roles
+      }
+      jwt.sign(payload, jwtSecret, (err, token) => {
+        res.send({
+          success: true,
+          token: token
+        })
+      })
+    } else {
+      // res.send('wrong credentials')
+      res.send({ success: false, message: 'Wrong credentials' })
+    }
+  } else {
+    // res.send('wrong credentials')10:34
+    res.send({ success: false, message: 'Wrong credentials' })
+  }
+  // res.send(user)
+})
+
+const createInitialUsers = async () => {
+  const total = await User.count({})
+  if (total === 0) {
+    const user = new User({
+      username: 'tulio',
+      password: '123456',
+      roles: ['restrito', 'admin']
+    })
+    await user.save()
+    const user2 = new User({
+      username: 'restrito',
+      password: '123456',
+      roles: ['restrito']
+    })
+    await user2.save()
+  }
+}
 
 // const series = [
 //   { name: 'Friends' },
@@ -23,6 +85,7 @@ app.use('/series', series)
 mongoose
   .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
+    createInitialUsers()
     app.listen(port, () => console.log('listening...'))
   })
   .catch(e => console.log(e))
--- routes/series.js
+++ routes/series.js
@@ -10,6 +10,30 @@ const express = require('express')
 const router = express.Router()
 const Serie = require('../models/serie')
 
+const jwt = require('jsonwebtoken')
+const jwtSecret = 'abc123abc123abc123'
+
+router.use(async (req, res, next) => {
+  // console.log(req)
+  const token = req.headers['x-access-token'] || req.body.token || req.query.token
+  if (token) {
+    try {
+      const payload = jwt.verify(token, jwtSecret)
+      console.log(payload)
+      if (payload.roles.indexOf('restrito') >= 0) {
+        next()
+      } else {
+        res.send({ success: false })
+      }
+    } catch (e) {
+      res.send({ success: false })
+    }
+  } else {
+    res.send({ success: false })
+  }
+  // next()
+})
+
 router.get('/', async (req, res) => {
   const series = await Serie.find({})
   res.send(series)
